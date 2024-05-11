9b91d347d68be20ea864b0bfd547d4d331be5ba6
Maria Kuvshinova <mkuvshinova96@gmail.com>
2024-05-11 22:47:06 +0500

    Add method PrintArray

--- Program.cs
+++ Program.cs
@@ -52,3 +52,22 @@ string[] CreateArrayOfStringsLengthLess3(string[] array)
     }
     return newArray;
 }
+
+// Функция вывода массива
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
