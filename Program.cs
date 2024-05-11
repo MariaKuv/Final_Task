5a01b26dac9216a001b1f09ad5d7d671c119bdd4
Maria Kuvshinova <mkuvshinova96@gmail.com>
2024-05-11 22:45:07 +0500

    Add method EnterStrings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
+// коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+// Задаем исходный массив
+
+string[] EnterStrings()
+{
+    Console.WriteLine("Введите количество строк");
+    int count = Convert.ToInt32(Console.ReadLine());
+    
+    string[] array = new string[count];
+    Console.WriteLine("Введите строки (через Enter)");
+    for (int i = 0; i < count; i++)
+    {
+    array[i] = Console.ReadLine();
+    }
+    return array;
+}
